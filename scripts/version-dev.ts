5de84f204ca7c5e0851c2a1d953cd536103df27d
Samual Norman <samualtnorman@gmail.com>
2020-12-08 14:47:55 +0000

    more tweaks

--- scripts/version-dev.ts
+++ scripts/version-dev.ts
@@ -4,14 +4,15 @@ import { readFile } from "fs/promises"
 Promise.all([
 	readFile("package.json", { encoding: "utf-8" })
 		.then(JSON.parse)
-		.then(({ version }: { version: string }) => version),
+		.then(({ version }: { version: string }) => version.split(".")),
 	execute("git rev-parse --short HEAD")
 		.then(({ stdout }) => stdout.trim())
-]).then(([ version, hash ]) =>
+]).then(([ version, hash ]) => {
+	version[1] = String(Number(version[1]) + 1)
+
 	execute(`npm version ${version}-${hash}`)
-		.then(({ stdout }) => stdout)
-		.then(console.log)
-)
+		.then(({ stdout }) => console.log(stdout))
+})
 
 function execute(command: string) {
 	return new Promise<{
