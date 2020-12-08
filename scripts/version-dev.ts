e3fec93d6d6db583383bdda8e273bc38723fa00a
Samual Norman <samualtnorman@gmail.com>
2020-12-08 15:02:12 +0000

    removed broken attempt at changing the semver myself now using official semver package

--- scripts/version-dev.ts
+++ scripts/version-dev.ts
@@ -1,18 +1,17 @@
 import { exec } from "child_process"
 import { readFile } from "fs/promises"
+import { inc } from "semver"
 
 Promise.all([
 	readFile("package.json", { encoding: "utf-8" })
 		.then(JSON.parse)
-		.then(({ version }: { version: string }) => version.split(".")),
+		.then(({ version }: { version: string }) => inc(version, "minor")),
 	execute("git rev-parse --short HEAD")
 		.then(({ stdout }) => stdout.trim())
-]).then(([ version, hash ]) => {
-	version[1] = String(Number(version[1]) + 1)
-
+]).then(([ version, hash ]) =>
 	execute(`npm version ${version}-${hash}`)
 		.then(({ stdout }) => console.log(stdout))
-})
+)
 
 function execute(command: string) {
 	return new Promise<{
