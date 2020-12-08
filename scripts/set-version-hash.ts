39869e2c3f984c74696043f699efae049a8bd1c5
Samual Norman <samualtnorman@gmail.com>
2020-12-08 11:27:59 +0000

    Update set-version-hash.ts

--- scripts/set-version-hash.ts
+++ scripts/set-version-hash.ts
@@ -1,13 +1,15 @@
 import { exec } from "child_process"
 import { readFile } from "fs/promises"
-import { PackageId } from "typescript"
 
-const packageJSON = readFile("package.json", { encoding: "utf-8" }).then(JSON.parse) as Promise<PackageId>
-const hash = execute("git rev-parse HEAD").then(({ stdout }) => stdout.slice(0, 7)) as Promise<string>
-
-Promise.all([ packageJSON, hash ]).then(async ([ packageJSON, hash ]) => {
-	await execute(`npm version ${packageJSON.version}-${hash}`).then(console.log, console.log)
-})
+Promise.all([
+	readFile("package.json", { encoding: "utf-8" })
+		.then(JSON.parse)
+		.then(({ version }: { version: string }) => version),
+	execute("git rev-parse --short HEAD")
+		.then(({ stdout }) => stdout)
+]).then(([ version, hash ]) =>
+	execute(`npm version ${version}-${hash}`).then(console.log, console.log)
+)
 
 function execute(command: string) {
 	return new Promise<{
