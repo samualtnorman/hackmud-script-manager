57de4ad678037e92300eb7f77097454b59c3c80b
Samual Norman <samualtnorman@gmail.com>
2021-06-15 23:41:10 +0100

    removed `strict: true` from typescript compiler options turns out all this does is give better type checking (not used here) and puts `"use strict";` at top of file (which causes hackmud to throw on upload)

--- src/index.ts
+++ src/index.ts
@@ -523,7 +523,6 @@ export async function processScript(script: string) {
 	const { outputText, diagnostics = [] } = transpileModule(script, {
 		compilerOptions: {
 			target: ScriptTarget.ES2015,
-			strict: true,
 			removeComments: true
 		},
 		reportDiagnostics: true
