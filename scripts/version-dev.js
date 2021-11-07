f7f4f0678bb57917b29edc18ccd40a566ee8595b
Samual Norman <samualtnorman@gmail.com>
2021-11-07 18:38:14 +0000

    update dependencies update devops and config stuff

--- scripts/version-dev.js
+++ scripts/version-dev.js
@@ -1,12 +1,23 @@
-import semver from "semver"
-import { promisify } from "util"
 import { exec as execute_ } from "child_process"
 import fs from "fs"
+import semver from "semver"
+import { promisify } from "util"
 
 const { readFile } = fs.promises
 const execute = promisify(execute_)
 
-;(async () => {
-	const [ packageJSONFile, { stdout: gitGetHashStdout } ] = await Promise.all([ readFile("package.json", { encoding: "utf-8" }), execute("git rev-parse --short HEAD") ])
-	console.log((await execute(`npm version ${semver.inc(JSON.parse(packageJSONFile).version, "minor")}-${gitGetHashStdout.trim()}`)).stdout)
-})()
+const [ packageJSONFile, { stdout: gitGetHashStdout } ] = await Promise.all([
+	readFile("package.json", { encoding: "utf-8" }),
+	execute("git rev-parse --short HEAD")
+])
+
+const { stdout, stderr } = await execute(
+	`npm version ${
+		semver.inc(JSON.parse(packageJSONFile).version, "minor")
+	}-${
+		gitGetHashStdout.trim()
+	}`
+)
+
+process.stdout.write(stdout)
+process.stderr.write(stderr)
