bbb173c206d9f329717c04b074994d7c2a1b5587
Samual Norman <samualtnorman@gmail.com>
2022-02-14 21:22:03 +0000

    massively simplify `postprocess()` to just be a bunch of `.replace()`s

--- src/processScript/postprocess.ts
+++ src/processScript/postprocess.ts
@@ -1,44 +1,16 @@
-import { findMatches, spliceString } from "@samual/lib"
-
 export function postprocess(code: string, seclevel: number, uniqueID: string) {
-	code = code
+	return code
 		.replace(/^function\s*\w+\(/, `function(`)
 		.replace(new RegExp(`\\$${uniqueID}\\$\\\\\\\\0\\$SC_DOLLAR\\$`, `g`), `SC\\$`)
 		.replace(new RegExp(`\\$${uniqueID}\\$\\\\\\\\0\\$DB_DOLLAR\\$`, `g`), `DB\\$`)
 		.replace(new RegExp(`\\$${uniqueID}\\$\\\\\\\\0\\$D\\$`, `g`), `__D_\\S`)
 		.replace(new RegExp(`\\$${uniqueID}\\$\\\\\\\\0\\$FMCL\\$`, `g`), `__FMCL\\_`)
 		.replace(new RegExp(`\\$${uniqueID}\\$\\\\\\\\0\\$G\\$`, `g`), `__G\\_`)
-
-	for (const { index, match } of [ ...findMatches(new RegExp(`\\$${uniqueID}\\$[\\w$]+`, `g`), code) ].reverse()) {
-		const [ type, ...arguments_ ] = match.slice(13).split(`$`)
-
-		switch (type) {
-			case `SUBSCRIPT`: {
-				code = spliceString(code, `#${`nlmhf`[seclevel]}s.${arguments_[0]}.${arguments_[1]}`, index, match.length)
-			} break
-
-			case `DEBUG`: {
-				code = spliceString(code, `#D`, index, match.length)
-			} break
-
-			case `FMCL`: {
-				code = spliceString(code, `#FMCL`, index, match.length)
-			} break
-
-			case `GLOBAL`: {
-				code = spliceString(code, `#G`, index, match.length)
-			} break
-
-			case `DB`: {
-				code = spliceString(code, `#db.${arguments_[0]}`, index, match.length)
-			} break
-
-			default:
-				throw new Error(`unknown type "${type}"`)
-		}
-	}
-
-	return code
+		.replace(new RegExp(`\\$${uniqueID}\\$SUBSCRIPT\\$(\\w+)\\$(\\w+)`, `g`), `#${`nlmhf`[seclevel]}s.$1.$2`)
+		.replace(new RegExp(`\\$${uniqueID}\\$DEBUG`, `g`), `#D`)
+		.replace(new RegExp(`\\$${uniqueID}\\$FMCL`, `g`), `#FMCL`)
+		.replace(new RegExp(`\\$${uniqueID}\\$GLOBAL`, `g`), `#G`)
+		.replace(new RegExp(`\\$${uniqueID}\\$DB\\$(\\w+)`, `g`), `#db.$1`)
 }
 
 export default postprocess
