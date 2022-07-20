7f0c754d2133be2148686b9cd6cb5e85561e3ccd
Samual Norman <samualtnorman@gmail.com>
2022-07-20 16:35:15 +0100

    fix process not exiting with status of child process on error

--- scripts/update-dependencies.js
+++ scripts/update-dependencies.js
@@ -3,6 +3,8 @@
 import { execFileSync as executeFileSync } from "child_process"
 import { readFileSync } from "fs"
 
+/** @typedef {Error & import("child_process").SpawnSyncReturns<null>} ExecuteFileSyncError */
+
 const packageConfig = JSON.parse(readFileSync(`package.json`, { encoding: `utf-8` }))
 
 const dependencies = [
@@ -12,15 +14,18 @@ const dependencies = [
 ]
 
 dependencies.splice(dependencies.indexOf(`@types/node`), 1)
+console.log(`> pnpm update ${dependencies.join(` `)} --latest`)
 
 try {
 	executeFileSync(`pnpm`, [ `update`, ...dependencies, `--latest` ], { stdio: `inherit` })
 } catch (error) {
-	process.exit(error.code)
+	process.exit(/** @type {ExecuteFileSyncError} */ (error).status)
 }
+
+console.log(`> pnpm update @types/node`)
 
 try {
 	executeFileSync(`pnpm`, [ `update`, `@types/node` ], { stdio: `inherit` })
 } catch (error) {
-	process.exit(error.code)
+	process.exit(/** @type {ExecuteFileSyncError} */ (error).status)
 }
