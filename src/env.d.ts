fdec23030a580e1570991c36aa494ecf1a1d8c06
Samual Norman <me@samual.uk>
2024-05-22 16:56:10 +0100

    update tsconfigs and account for missing `performance` API in dts

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,9 @@ declare module "@babel/plugin-*" {
 
 	export default value
 }
+
+// Taken from https://github.com/thw0rted/DefinitelyTyped/blob/542cacd0803f50d8537a94b2dfa3e3e125c837ac/types/node/perf_hooks.d.ts#L608
+/** `performance` is a global reference for `require('perf_hooks').performance`
+  * https://nodejs.org/api/globals.html#performance
+  * @since v16.0.0 */
+var performance: typeof globalThis extends { onmessage: any, performance: infer T } ? T : typeof import("perf_hooks").performance
