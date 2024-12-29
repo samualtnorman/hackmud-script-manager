8ea4b63e6d3e56e76d1bb8b9375f9e9c6382d876
Samual <me@samual.uk>
2024-12-29 18:34:15 +0000

    add `MongoObjectId` to `MongoId` type union

--- env.d.ts
+++ env.d.ts
@@ -720,7 +720,7 @@ type MongoTypeStringsToTypes = {
 
 type MongoTypeString = keyof MongoTypeStringsToTypes
 type MongoTypeNumber = -1 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 16 | 17 | 18 | 19 | 127
-type MongoId = Exclude<MongoPrimitive, null> | MongoObject
+type MongoId = Exclude<MongoPrimitive, null> | MongoObjectId | MongoObject
 type MongoQueryId = Exclude<MongoPrimitive, null> | MongoQueryObject
 type MongoDocument = MongoObject & { _id?: MongoId }
 
