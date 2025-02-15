c02c6a26541a818ddfdfeb212efc74989894ca75
Samual <me@samual.uk>
2025-02-15 16:53:38 +0000

    fix `HERE`s being off in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,19 @@
 import type { ViteUserConfig } from "vitest/config"
+// @ts-expect-error it does exist, it just doesn't have typings
+import babelPluginSyntaxTypescript from "@babel/plugin-syntax-typescript"
 import { babel } from "@rollup/plugin-babel"
 import { babelPluginHere } from "babel-plugin-here"
 
 export default {
 	test: { includeSource: [ `src/**/*.ts` ] },
-	plugins: [ babel({ babelHelpers: `bundled`, extensions: [ `.ts` ], plugins: [ babelPluginHere() ] }) as any ]
+	plugins: [
+		{
+			...babel({
+				babelHelpers: `bundled`,
+				extensions: [ `.ts` ],
+				plugins: [ babelPluginSyntaxTypescript, babelPluginHere() ]
+			}) as any,
+			enforce: `pre`
+		}
+	]
 } satisfies ViteUserConfig
