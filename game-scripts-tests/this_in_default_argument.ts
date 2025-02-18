87bf5c08a2e111d09b499ca5534929954cf52e74
SarahIsWeird <morningcrafter0603@gmail.com>
2025-02-18 16:48:57 +0100

    Add failing test for `this` in argument default

---
+++ game-scripts-tests/this_in_default_argument.ts
@@ -0,0 +1,35 @@
+export default ({ expect }: typeof import('vitest')) => {
+    class Foo {
+        declare foo: number;
+
+        constructor() {
+            this.foo = 1
+        }
+
+        bar() {
+            return 2
+        }
+
+        baz(a = this.foo, b = this.bar()) {
+            return a + b
+        }
+    }
+
+    const obj = {
+        foo: 1,
+        bar() {
+            return 2
+        },
+        // @ts-ignore
+        baz(a = this.foo, b = this.bar()) {
+            return a + b
+        }
+    }
+
+    const foo = new Foo();
+    expect(foo.baz(3, 4)).toBe(7) // Is this the real life?
+    expect(foo.baz()).toBe(3) // Is this just fantasy?
+
+    expect(obj.baz(3, 4)).toBe(7) // Caught in a landslide
+    expect(obj.baz()).toBe(3) // No escape from reality
+}
