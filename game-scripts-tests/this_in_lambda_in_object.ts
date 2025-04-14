67dc9bb8cdc25e10376660d0d593e6949de0ec4f
SarahIsWeird <morningcrafter0603@gmail.com>
2025-04-14 23:00:37 +0200

    Fix formatting

--- game-scripts-tests/this_in_lambda_in_object.ts
+++ game-scripts-tests/this_in_lambda_in_object.ts
@@ -1,23 +1,23 @@
 export default ({ expect }: typeof import('vitest')) => {
-    const myObject = {
-        a: 0,
-        b: function () {
-            this.a++
-            return this.a
-        },
-        c: function (value: number = this.a) {
-            this.a = value + 1
-        },
-        d: function ({ value = this.a }: { value?: number }) {
-            this.a = value + 1
-        }
-    }
+	const myObject = {
+		a: 0,
+		b: function () {
+			this.a++
+			return this.a
+		},
+		c: function (value: number = this.a) {
+			this.a = value + 1
+		},
+		d: function ({ value = this.a }: { value?: number }) {
+			this.a = value + 1
+		}
+	}
 
-    expect(myObject.a).toBe(0)
-    myObject.b()
-    expect(myObject.a).toBe(1)
-    myObject.c()
-    expect(myObject.a).toBe(2)
-    myObject.d({})
-    expect(myObject.a).toBe(3)
+	expect(myObject.a).toBe(0)
+	myObject.b()
+	expect(myObject.a).toBe(1)
+	myObject.c()
+	expect(myObject.a).toBe(2)
+	myObject.d({})
+	expect(myObject.a).toBe(3)
 }
